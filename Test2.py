bea21bdfd5e30dc992bf014487683e5b14194dd8
WinPC <you@example.com>
2020-08-14 09:53:17 +0800

    update

--- Test2.py
+++ Test2.py
@@ -4,29 +4,18 @@
 #     Str = "nohup python3 -u Crawler_Parameter.py " + str(i*500) + " 500 > Crawler_" + str(i*500) +"_500.log 2>&1 &"
 #     print(Str)
 import csv
+import os
 filename = 'pdf_process.sh'
+root_path = os.getcwd()
 with open(filename, 'w') as file_object:
-    with open('nlpedia-db.csv', 'r', encoding='UTF-8') as f:
+    with open('PDF_Name.csv', 'r', encoding='UTF-8') as f:
         reader = csv.reader(f)
-
         idx = 0
 
         for row in reader:
             idx+=1
             if idx>1:
-                url0 = row[7]
-                url = url0.split(" ")[0]
-                if url!="None":
+                validation = row[1]
+                if validation!="None":
+                    command = "java -Xmx6g -jar science-parse-cli-assembly-3.0.1.jar" + row[2] + '''"$1"''' + "-f " + "./" + row[3] + "/" + row[0] + "_parse.json"
 
-                    # print("wget " + url)
-                    dir = row[0] + "_" + row[1] + "_" + row[2] + "_Get"
-                    # file_object.write("mkdir " + dir +"\n")
-                    file_object.write("cd " + dir +"\n")
-                    # file_object.write("wget " + url +"\n")
-
-                    json_name = row[0] + "_parse.json"
-                    # file_object.write("java -Xmx6g -jar science-parse-cli-assembly-3.0.1.jar  "$1" -f paper.json")
-                    file_object.write("cd " + ".." +"\n")
-                else:
-                    dir = row[0] + "_" + row[1] + "_" + row[2] + "_None"
-                    file_object.write("mkdir " + dir +"\n")
