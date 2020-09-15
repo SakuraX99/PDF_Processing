d7066d5a5258c432a9a0dec386e6fc26e9739654
WinPC <you@example.com>
2020-09-15 18:20:27 +0800

    upload

--- valid_count.py
+++ valid_count.py
@@ -4,7 +4,14 @@ import os
 
 
 count = 0
-for root, dirs, files in os.walk("/usr2/home/pliu3/PDF_Processing"):
+with open("valid_inspect.txt", 'w', encoding='UTF-8') as f:
+    for root, dirs, files in os.walk("/usr2/home/pliu3/PDF_Processing"):
 
-    for dir in dirs:
-       print(dir)
+        for dir in dirs:
+            f.write(dir + " ")
+            for root1, dirs1, files1 in os.walk(dir):
+                for file in files1:
+                    f.write(file + ":" + os.path.getsize(dir+"/"+file)+" ")
+            f.write("\n")
+
+
